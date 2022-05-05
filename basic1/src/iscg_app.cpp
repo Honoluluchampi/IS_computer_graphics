09cfb131158f61628c3b4f82f14ac458e63568b1
Honoluluchampi <msd13aj@gmail.com>
2022-05-05 18:51:40 +0900

    point light manager

--- basic1/src/iscg_app.cpp
+++ basic1/src/iscg_app.cpp
@@ -12,7 +12,7 @@ void IscgApp::createActor()
   auto actor = std::make_unique<hnll::HgeActor>();
   auto lightComp = hnll::PointLightComponent::createPointLight(actor->getId(), 0.0f, 0.5f, glm::vec3{.1f, .1f, 1.f});
   auto dragComp = std::make_shared<DraggableComponent>(*dragManager, lightComp->getTransform());
-  actor->addSharedComponent(std::move(lightComp));
+  this->addPointLight(actor, lightComp);
   actor->addSharedComponent(dragComp);
   dragManager->addDragComps(dragComp);
   dragManager_.reset(dragManager);
